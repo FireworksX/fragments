a0092cbf04e09542c780002dcfc1c6e736aef22d
Kovalev Vasily <32467326+KovalevVasiliy@users.noreply.github.com>
2025-07-24 16:59:35 +0300

    Cache (#89)

--- apps/backend/service/services/core/routes/filter.py
+++ apps/backend/service/services/core/routes/filter.py
@@ -9,7 +9,9 @@ from services.core.routes.middleware import Context
 from .schemas.release_condition import AllFiltersGet, CountryGet, DeviceType, OSType, RegionGet
 from .schemas.user import AuthPayload
 from conf.settings import logger
+from utils.lru_cache import CustomLRUCache
 
+geo_cache = CustomLRUCache(maxsize=128)
 
 async def get_all_filters(
     info: strawberry.Info[Context],
@@ -24,18 +26,31 @@ async def get_all_filters(
     os_types: List[OSType] = [v for v in OSType]
     device_types: List[DeviceType] = [v for v in DeviceType]
     
-    logger.debug(f"Getting geo locations with countries filter: {countries_filter} and regions filter: {regions_filter}")
     locations = get_geo_locations(db, countries_filter, regions_filter)
-
     logger.debug("Building country dictionary")
-    country_dict: Dict[str, Dict[str, List[str]]] = {}
-    for location in locations:
-        if location.country not in country_dict and location.country is not None:
-            country_dict[location.country] = {}
-        if location.region not in country_dict[location.country] and location.region is not None:
-            country_dict[location.country][location.region] = []
-        if location.country is not None and location.region is not None:
-            country_dict[location.country][location.region].append(location.city)
+    # Create cache key from the filter values
+    cache_key = (
+        tuple(countries_filter) if countries_filter else None,
+        tuple(regions_filter) if regions_filter else None
+    )
+    
+    # Try to get from cache
+    cached_results = geo_cache.get(cache_key)
+    if cached_results is not None:
+        logger.debug("Returning cached country dictionary")
+        country_dict = cached_results
+    else:
+        country_dict: Dict[str, Dict[str, List[str]]] = {}
+        for location in locations:
+            if location.country not in country_dict and location.country is not None:
+                country_dict[location.country] = {}
+            if location.region not in country_dict[location.country] and location.region is not None:
+                country_dict[location.country][location.region] = []
+            if location.country is not None and location.region is not None:
+                country_dict[location.country][location.region].append(location.city)
+        
+        # Cache the results
+        geo_cache.put(cache_key, country_dict)
 
     logger.debug("Building countries list")
     countries: List[CountryGet] = []
