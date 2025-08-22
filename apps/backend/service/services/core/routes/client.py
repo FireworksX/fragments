0965e6737434f8fae58f1967a1e6d733e192b193
Kovalev Vasily <32467326+KovalevVasiliy@users.noreply.github.com>
2025-08-22 15:21:14 +0300

    Fix roles (#123)

--- apps/backend/service/services/core/routes/client.py
+++ apps/backend/service/services/core/routes/client.py
@@ -18,18 +18,10 @@ from database.models import Project
 
 from .campaign import CampaignStatus, get_campaigns_by_area_id_db
 from .middleware import ClientInfo, Context
-from .project import get_user_role_in_project
 from .schemas.client import ClientAreaGet, ClientHistoryEventType, ClientHistoryPost
 from .schemas.feature_flag import RotationType
-from .schemas.user import UserRole
 from .schemas.variant import VariantGet, VariantStatus
 from .variant import variant_db_to_variant
-
-
-async def read_permission(db: Session, user_id: int, project_id: int) -> bool:
-    logger.info(f"Checking read permission for user {user_id} in project {project_id}")
-    role: Optional[UserRole] = await get_user_role_in_project(db, user_id, project_id)
-    return role is not None
 
 
 def set_user_id_cookie(info: strawberry.Info[Context], client: Client, max_age: int = 3600) -> None:
